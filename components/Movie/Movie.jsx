1d2607e80425c99a83e6488d73ce03e394441130
suraj009 <surajrajmane18@gmail.com>
2021-04-26 20:28:03 +0530

    improved spacing and font-style

--- components/Movie/Movie.jsx
+++ components/Movie/Movie.jsx
@@ -9,8 +9,8 @@ const Movie = ({movie}) => {
                         <img src={movie.Poster} alt="movieImage not found" />
                     </div>
                     <div className="col-md-6">
-                        <h5 className="mt-3">{movie.Title}</h5>
-                        <p >{movie.Year}</p>
+                        <h5 className="mt-3 font-weight-bold">{movie.Title}</h5>
+                        <p className="mt-2">{movie.Year}</p>
                     </div>
                 </div>
 
